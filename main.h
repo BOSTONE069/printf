cefd982000eb885d4057530d195114a5a9b3ffa0
rhemney <bostoneochieng69@gmail.com>
2022-05-19 06:16:50 -0700

    Main Header File

---
+++ main.h
@@ -0,0 +1,32 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+int _printf(const char *format, ...);
+void print_number(int n, int *count);
+
+/**
+ * struct vartype - struct vartype
+ *
+ * @vartype: type of variable to be printed
+ * @f: the function associated
+ */
+
+typedef struct vartype
+{
+	char *vartype;
+	int (*f)(va_list);
+} var_t;
+
+int c_func(va_list args);
+int s_func(va_list args);
+int perc_func(va_list args);
+int i_func(va_list args);
+int d_func(va_list args);
+int b_func(va_list args);
+int rev_func(va_list args);
+int rot_func(va_list args);
+
+#endif
