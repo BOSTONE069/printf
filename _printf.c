b8c5529bbf4b1fb42b5e77ed92a75795faac012c
overwatch <nzioki.grace@gmail.com>
2022-05-22 22:37:48 +0300

    print format

--- _printf.c
+++ _printf.c
@@ -8,42 +8,42 @@
  */
 int _printf(const char *format, ...)
 {
-	int sum = 0;
+	int bytes = 0;
 	va_list ap;
 	char *p, *start;
-	params_t params = PARAMS_INIT;
+	params_t params = PARAMS_INIT; /* initialize struct to all 0 */
 
 	va_start(ap, format);
 
-	if (!format || (format[0] == '%' && !format[1]))
+	if (!format || (format[0] == '%' && !format[1])) /* empty or %\0 */
 		return (-1);
-	if (format[0] == '%' && format[1] == ' ' && !format[2])
+	if (format[0] == '%' && format[1] == ' ' && !format[2]) /* % \0 */
 		return (-1);
 	for (p = (char *)format; *p; p++)
 	{
 		init_params(&params, ap);
-		if (*p != '%')
+		if (*p != '%') /* normal characters */
 		{
-			sum += _putchar(*p);
+			bytes += _putchar(*p);
 			continue;
 		}
-		start = p;
-		p++;
-		while (get_flag(p, &params)) /* while char at p is flag char */
+		start = p; /* % */
+		p++; /* what follows % */
+		while (get_flag(p, &params)) /* while char p a is flag char */
 		{
-			p++; /* next char */
+			p++;
 		}
 		p = get_width(p, &params, ap);
 		p = get_precision(p, &params, ap);
 		if (get_modifier(p, &params))
 			p++;
 		if (!get_specifier(p))
-			sum += print_from_to(start, p,
+			bytes += print_from_to(start, p,
 				params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
-			sum += get_print_func(p, ap, &params);
+			bytes += get_print_func(p, ap, &params);
 	}
 	_putchar(BUF_FLUSH);
 	va_end(ap);
-	return (sum);
+	return (bytes);
 }
