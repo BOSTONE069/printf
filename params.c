1cbf899dac2e30fbfc9c883f44569bffb419da85
overwatch <nzioki.grace@gmail.com>
2022-05-22 22:30:54 +0300

    reset all param_struct elements to false

--- params.c
+++ params.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * init_params - clears struct fields and reset buf
+ * init_params - clears struct fields and resets buf
  * @params: the parameters struct
  * @ap: the argument pointer
  *
